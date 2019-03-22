f23098e364c9aefd3de3719bab3d2c7d85ca3045
Mahesh Sammatashetti <maheshsam@cybage.com>
2019-03-22 15:01:22 +0530

    Initial Commit

---
+++ CommonMethods/CommonMethods.h
@@ -0,0 +1,19 @@
+//
+//  CommonMethods.h
+//  CommonMethods
+//
+//  Created by Mahesh Sammatashetti on 22/03/19.
+//  Copyright © 2019 Cybage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonMethods.
+FOUNDATION_EXPORT double CommonMethodsVersionNumber;
+
+//! Project version string for CommonMethods.
+FOUNDATION_EXPORT const unsigned char CommonMethodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonMethods/PublicHeader.h>
+
+
---
+++ CommonMethods/General.swift
@@ -0,0 +1,48 @@
+//
+//  General.swift
+//  CommonMethods
+//
+//  Created by Mahesh Sammatashetti on 22/03/19.
+//  Copyright © 2019 Cybage. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class General
+{
+    public func randomNumber() -> Int
+    {
+        let number = arc4random_uniform(100)
+        print("Number: \(Int(number))")
+        
+        return Int(number)
+    }
+    
+    internal func getRandomColor1() -> UIColor
+    {
+        let randomRed: CGFloat = CGFloat(drand48())
+        let randomGreen: CGFloat = CGFloat(drand48())
+        let randomBlue: CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+    
+    private func getRandomColor2() -> UIColor
+    {
+        let randomRed: CGFloat = CGFloat(drand48())
+        let randomGreen: CGFloat = CGFloat(drand48())
+        let randomBlue: CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+    
+    fileprivate func getRandomColor3() -> UIColor
+    {
+        let randomRed: CGFloat = CGFloat(drand48())
+        let randomGreen: CGFloat = CGFloat(drand48())
+        let randomBlue: CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+}
